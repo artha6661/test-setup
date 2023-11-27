45bc355ce85c42eb4f27174a74f332180fb4c73c
artha bastanta <arthabastanta03@gmail.com>
2023-11-27 18:08:43 +0700

    First merge

---
+++ src/main/java/com/arthampan/demo/TampanApplication.java
@@ -0,0 +1,13 @@
+package com.arthampan.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TampanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TampanApplication.class, args);
+	}
+
+}
